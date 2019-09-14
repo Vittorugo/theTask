537352abe7419475d41f14e7ae89b3ff0749ebb5
Victtor Hugo <hgloureirocosta@gmail.com>
2019-09-14 18:01:15 -0300

    separar por grupos msgs que respondidas em menos de dois minutos

--- logic_assignment/index.py
+++ logic_assignment/index.py
@@ -21,55 +21,53 @@ class user():
 def user_msg( username, msg ):
 
 
-
     obj_user_msg = user(username, msg)
+
     list_json.append(obj_user_msg)
     #lista de usuários ...
 
 
-
-
-
 obj_msg = {}
+list_Sub_mensage = []
 
 for usuario in dados_json:
 
     fast_msg = []
-    tempo_msg_vez = float(usuario['ts'])
-    ts_primeira_msg = datetime.fromtimestamp(tempo_msg_vez)
+    time_msg = float(usuario['ts'])
+    ts_first_msg = datetime.fromtimestamp(time_msg)
 
 
-    usuario_msg = []
+    list_user_msg = []
     usuario_consultado = usuario['user']
 
     if usuario_consultado not in lista_de_usuarios:
 
         lista_de_usuarios.append(usuario_consultado)
 
+        for nome in dados_json:
+
+            if  usuario_consultado == nome['user']:
+
+                list_user_msg.append(nome['text']) # separando as msgs do usuario...
+
+        user_msg(usuario_consultado, list_user_msg)
+
+
+    else:
+
         # timestamp da primeira msg ...
         timestamp_first_msg = float(usuario['ts'])
         ts_first = datetime.fromtimestamp(timestamp_first_msg)
 
-        for nome in dados_json:
+        for  time_sub_msg in dados_json:
 
-
-            timestamp_last_msg =  float(nome['ts'])  #tempo da última msg ...
+            timestamp_last_msg = float(time_sub_msg['ts'])  # tempo da última msg ...
             ts_last = datetime.fromtimestamp(timestamp_last_msg)
 
+            if (ts_last.minute - ts_first.minute) < 2:
+                fast_msg.append(time_sub_msg['text'])  # adicionado msgs respondidas em menos de 2 minutos ...
 
-            if (ts_last.minute - ts_first.minute) < 2 :
+        obj_msg[usuario['ts']] = fast_msg  # objeto com chave = horário da mensagem e valor = mensagens respondidas antes de 2 minutos.
 
-                fast_msg.append(nome['text'])  # adicionado msgs respondidas em menos de 2 minutos ...
+        list_Sub_mensage.append(obj_msg)
 
-            if  usuario_consultado == nome['user']:
-
-                usuario_msg.append(nome['text']) # separando as msgs do usuario...
-
-
-        obj_msg[usuario['ts']] = fast_msg #objeto com chave = horário da mensagem e valor = mensagens respondidas antes de 2 minutos.
-
-        user_msg(usuario_consultado, usuario_msg)
-
-
-for i in list_json:
-    print(i.mensages)
